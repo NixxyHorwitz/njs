a5181de3c84b02b35b99d4ed2eb9fec0a7c6a64a
NixxyHorwitz <nixhorwitz@gmail.com>
2026-01-08 02:50:16 +0700

    apdet

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['i.imgur.com'],
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
